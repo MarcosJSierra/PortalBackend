1cdef95ce2da2fad82dc1bdcbf4ee9486e64a216
Marcos Sierra <marcosjspac@gmail.com>
2022-01-17 17:31:21 -0600

    Agregando proyecto base

---
+++ src/main/java/gt/org/marcos/PortalBackend/PortalBackendApplication.java
@@ -0,0 +1,13 @@
+package gt.org.marcos.PortalBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gt/org/marcos/PortalBackend/PortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.org.marcos.PortalBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
